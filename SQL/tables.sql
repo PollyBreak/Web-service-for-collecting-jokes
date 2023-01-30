847e370963635fd6fd3d5a8212361ee0ebcf32c4
beneft <letun_igor007@mail.ru>
2023-01-30 23:10:05 +0600

    Update tables.sql

--- SQL/tables.sql
+++ SQL/tables.sql
@@ -1,19 +1,31 @@
-сreate database funny_db;
+create database funny_db;
+use funny_db;
+
+create table category_jokes(
+    category_id int PRIMARY KEY AUTO_INCREMENT,
+    name text
+);
 
 create table jokes(
 id int PRIMARY KEY AUTO_INCREMENT,
+category int,
 text text,
-rating int default 0);
+rating int default 0,
+foreign key (category) references category_jokes(category_id));
 
 create table memes(
 id int PRIMARY KEY AUTO_INCREMENT,
 url text,
 rating int default 0);
 
-create table new_jokes(
+create table new_memes(
 id int PRIMARY KEY AUTO_INCREMENT,
 url text,
-decision char(30));
+decision bool);
 
-
-
+create table new_jokes(
+id int PRIMARY KEY AUTO_INCREMENT,
+category int,
+text text,
+decision bool,
+foreign key (category) references category_jokes(category_id));
