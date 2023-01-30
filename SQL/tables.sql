f6847424bd46d6480d82f36dc757ba9716d053f7
Ban qup <ali.abdulla.nurtas@gmail.com>
2023-01-30 16:30:24 +0600

    added category jokes, column category_id to jokes

--- SQL/tables.sql
+++ SQL/tables.sql
@@ -2,6 +2,7 @@
 
 create table jokes(
 id int PRIMARY KEY AUTO_INCREMENT,
+category_id int,
 text text,
 rating int default 0);
 
@@ -14,6 +15,11 @@ create table new_jokes(
 id int PRIMARY KEY AUTO_INCREMENT,
 url text,
 decision char(30));
+
+create table category_jokes(
+    id int PRIMARY KEY AUTO_INCREMENT,
+    name text
+);
 
 
 
