d3ffe39e2778fd5f699e0a8759b5a022305275d5
PollyBreak <mspolinko@gmail.com>
2023-01-31 13:22:30 +0600

    create controller for categories
    let's make rest app
    (delete all views, thymeleaf and so on)

---
+++ Web service for collecting jokes/src/main/java/com/se2212/web_service_for_jokes/controller/CategoriesRestController.java
@@ -0,0 +1,27 @@
+package com.se2212.web_service_for_jokes.controller;
+
+import com.se2212.web_service_for_jokes.entity.JokeCategory;
+import com.se2212.web_service_for_jokes.service.JokeCategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class CategoriesRestController {
+
+    @Autowired
+    private JokeCategoryService jokeCategoryService;
+    @GetMapping("/categories")
+    public List<JokeCategory> showAllCategories(){
+        List<JokeCategory> categories = jokeCategoryService.getAllCategories();
+        return categories;
+    }
+
+
+
+
+}
