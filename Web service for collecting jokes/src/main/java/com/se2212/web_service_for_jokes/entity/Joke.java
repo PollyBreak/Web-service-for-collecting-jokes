b78b52742562b36d61d1b69e12841f2272aa6fd6
Madina <123440519+sleepingforest100@users.noreply.github.com>
2023-02-02 13:01:35 +0600

    added jokes in russian and english languages

--- Web service for collecting jokes/src/main/java/com/se2212/web_service_for_jokes/entity/Joke.java
+++ Web service for collecting jokes/src/main/java/com/se2212/web_service_for_jokes/entity/Joke.java
@@ -1,4 +1,43 @@
 package com.se2212.web_service_for_jokes.entity;
 
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+
+@Entity
+@Table(name = "jokes")
 public class Joke {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "category_id")
+    private int id;
+
+    @NotBlank(message = "Name cannot be blank")
+    @Column(name = "name")
+    private String name;
+
+    public Joke(String name) {
+        this.name = name;
+    }
+
+    public Joke() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Категория: " + name + '\'';
+    }
 }
+
