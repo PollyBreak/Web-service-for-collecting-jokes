a7d5e12ae189a4dfc695825a837a483740c6b0c8
PollyBreak <mspolinko@gmail.com>
2023-02-04 01:43:47 +0600

    update JokeRestController
    delete useless imports
    updated methods delete and update (changed request method)

--- Web service for collecting jokes/src/main/java/com/se2212/web_service_for_jokes/controller/JokesRestController.java
+++ Web service for collecting jokes/src/main/java/com/se2212/web_service_for_jokes/controller/JokesRestController.java
@@ -1,10 +1,7 @@
 package com.se2212.web_service_for_jokes.controller;
 
 import com.se2212.web_service_for_jokes.entity.Joke;
-import com.se2212.web_service_for_jokes.entity.JokeCategory;
-import com.se2212.web_service_for_jokes.service.JokeCategoryService;
 import com.se2212.web_service_for_jokes.service.JokesService;
-import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -28,14 +25,12 @@ public class JokesRestController {
     public void saveJoke(@RequestBody Joke joke){
         jokesService.saveJoke(joke);
     }
-    @PostMapping("/jokes/{id}")
-    public Joke updateJoke(@PathVariable int id,@RequestBody Joke newjoke){
-        jokesService.getJokeById(id); //if trying to update when does not exist it adds, so i just crash the whole thing
-        newjoke.setId(id);
-        jokesService.saveJoke(newjoke);
-        return newjoke;
+    @PutMapping("/jokes")
+    public Joke updateJoke(@RequestBody Joke updatedjoke){
+        jokesService.saveJoke(updatedjoke);
+        return updatedjoke;
     }
-    @GetMapping("/jokes/{id}/delete")
+    @DeleteMapping("/jokes/{id}")
     public List<Joke> deleteJoke(@PathVariable int id){
         jokesService.deleteJoke(id);
         return showAllJokes();
