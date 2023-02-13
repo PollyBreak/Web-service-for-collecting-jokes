f3f3c2755aa0622f6e2ced2b3e463a60175247dd
PollyBreak <mspolinko@gmail.com>
2023-02-14 01:52:23 +0600

    add data to insert

---
+++ OTHERS/SQL/insert_data.sql
@@ -0,0 +1,10 @@
+insert into db_for_fun.roles (role) values ('USER'), ('ADMIN');
+
+insert into db_for_fun.users (username, first_name, email, password) values ('PollyBreak', 'Polina', 'polly@mail.ru',
+ '{bcrypt}$2y$10$OEj39LHJek6/DHHSExSwFeq4jjiEALpVgBnswZobiXEXDIGjuJsWa');
+ 
+insert into db_for_fun.user_roles values (1,1);
+insert into db_for_fun.user_roles values (1,2);
+ 
+ 
+commit;
