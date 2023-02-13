63833264ba0599edf4ebb8202329a6fb05406a71
PollyBreak <mspolinko@gmail.com>
2023-02-14 05:17:14 +0600

    added missing bracket

--- Web service for collecting jokes/src/main/java/com/se2212/web_service_for_jokes/security/JwtTokenProvider.java
+++ Web service for collecting jokes/src/main/java/com/se2212/web_service_for_jokes/security/JwtTokenProvider.java
@@ -41,7 +41,7 @@ public class JwtTokenProvider {
 
     public String createToken(String username, List<Role> roles){
         Claims claims = Jwts.claims().setSubject(username);
-        claims.put("roles", getRoleNames(roles);
+        claims.put("roles", getRoleNames(roles));
         Date createDate = new Date();
         Date finishTime = new Date(createDate.getTime() + validity);
 
