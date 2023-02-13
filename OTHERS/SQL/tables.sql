8feab6c9ec807b29e314174d48ee4ba6e17b432d
PollyBreak <mspolinko@gmail.com>
2023-02-14 01:52:50 +0600

    create tables for users and roles

--- OTHERS/SQL/tables.sql
+++ OTHERS/SQL/tables.sql
@@ -16,6 +16,27 @@ create table memes(
 id int PRIMARY KEY AUTO_INCREMENT,
 url text);
 
+create table db_for_fun.users(
+user_id int PRIMARY KEY AUTO_INCREMENT,
+username varchar(15) UNIQUE NOT NULL,
+first_name varchar(15) NOT NULL,
+email varchar(30) UNIQUE NOT NULL,
+password varchar(300) NOT NULL,
+status BOOL default 1 NOT NULL
+);
+
+create table db_for_fun.roles(
+role_id int PRIMARY KEY AUTO_INCREMENT,
+role varchar(20) UNIQUE NOT NULL
+);
+
+create table db_for_fun.user_roles(
+user_id int,
+role_id int,
+foreign key (user_id) references db_for_fun.users(user_id),
+foreign key (role_id) references db_for_fun.roles(role_id)
+)
+
 /*
 create table new_memes(
 id int PRIMARY KEY AUTO_INCREMENT,
