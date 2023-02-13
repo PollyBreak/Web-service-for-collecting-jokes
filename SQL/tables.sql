19dde72a8066784e82252ff555354f26786328f6
PollyBreak <mspolinko@gmail.com>
2023-02-14 01:44:24 +0600

    add data to insert

--- SQL/tables.sql
+++ SQL/tables.sql
@@ -19,6 +19,31 @@ id int PRIMARY KEY AUTO_INCREMENT,
 url text,
 rating int default 0);
 
+<<<<<<< Updated upstream:SQL/tables.sql
+=======
+create table db_for_fun.users(
+user_id int PRIMARY KEY AUTO_INCREMENT,
+username varchar(15) UNIQUE NOT NULL,
+first_name varchar(15) NOT NULL,
+email varchar(30) UNIQUE NOT NULL,
+password varchar(30) NOT NULL,
+status BOOL default 1 NOT NULL
+);
+
+create table db_for_fun.roles(
+role_id int PRIMARY KEY AUTO_INCREMENT,
+role varchar(20) UNIQUE NOT NULL
+);
+
+create table db_for_fun.user_roles(
+user_id int,
+role_id int,
+foreign key (user_id) references db_for_fun.users(user_id),
+foreign key (role_id) references db_for_fun.roles(role_id)
+)
+
+/*
+>>>>>>> Stashed changes:OTHERS/SQL/tables.sql
 create table new_memes(
 id int PRIMARY KEY AUTO_INCREMENT,
 url text,
