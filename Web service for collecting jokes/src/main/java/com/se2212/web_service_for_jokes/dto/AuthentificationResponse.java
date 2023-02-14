d4f95616ffba9621bec74e9bed2ba98e3ad7a233
PollyBreak <mspolinko@gmail.com>
2023-02-14 20:34:12 +0300

    move to dto folder

---
+++ Web service for collecting jokes/src/main/java/com/se2212/web_service_for_jokes/dto/AuthentificationResponse.java
@@ -0,0 +1,15 @@
+package com.se2212.web_service_for_jokes.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class AuthentificationResponse {
+    private String token;
+
+}
