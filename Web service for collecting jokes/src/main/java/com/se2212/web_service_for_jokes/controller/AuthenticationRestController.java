2429d34f8c9c05b2f7ea3ad07c52ccf517a5cbae
PollyBreak <mspolinko@gmail.com>
2023-02-14 20:10:47 +0300

    rename jwtUser to User

--- Web service for collecting jokes/src/main/java/com/se2212/web_service_for_jokes/controller/AuthenticationRestController.java
+++ Web service for collecting jokes/src/main/java/com/se2212/web_service_for_jokes/controller/AuthenticationRestController.java
@@ -1,23 +1,15 @@
 package com.se2212.web_service_for_jokes.controller;
 
-import com.se2212.web_service_for_jokes.dto.AuthenticationRequestDto;
-import com.se2212.web_service_for_jokes.entity.User;
+import com.se2212.web_service_for_jokes.dto.AuthentificationRequest;
+import com.se2212.web_service_for_jokes.dto.AuthentificationResponse;
+import com.se2212.web_service_for_jokes.dto.RegisterRequest;
 import com.se2212.web_service_for_jokes.security.*;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.BadCredentialsException;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.AuthenticationException;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.HashMap;
-import java.util.Map;
 
 @RestController
 @RequestMapping(value = "/api/v1/auth")
--- Web service for collecting jokes/src/main/java/com/se2212/web_service_for_jokes/repository/UserRepository.java
+++ Web service for collecting jokes/src/main/java/com/se2212/web_service_for_jokes/repository/UserRepository.java
@@ -1,12 +1,10 @@
 package com.se2212.web_service_for_jokes.repository;
 
 import com.se2212.web_service_for_jokes.entity.User;
-import com.se2212.web_service_for_jokes.security.JwtUser;
-import io.jsonwebtoken.Jwt;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
-public interface UserRepository extends JpaRepository<JwtUser,Integer> {
-    public Optional<JwtUser> findByUsername(String username);
+public interface UserRepository extends JpaRepository<User,Integer> {
+    public Optional<User> findByUsername(String username);
 }
