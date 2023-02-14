c9e66bc360ba541448b34f97a88120d22a1ae83e
PollyBreak <mspolinko@gmail.com>
2023-02-14 20:11:05 +0300

    updated entity Role

--- Web service for collecting jokes/src/main/java/com/se2212/web_service_for_jokes/entity/Role.java
+++ Web service for collecting jokes/src/main/java/com/se2212/web_service_for_jokes/entity/Role.java
@@ -1,9 +1,13 @@
 package com.se2212.web_service_for_jokes.entity;
 
 import jakarta.persistence.*;
+import lombok.*;
 
 import java.util.List;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
 @Table(name="roles")
 public class Role {
@@ -16,34 +20,5 @@ public class Role {
 
     @ManyToMany(mappedBy = "roles")
     private List<User> users;
-    public Role(){}
-    public Role(String name) {
-        this.name = name;
-    }
-    public String getName() {
-        return name;
-    }
-    public void setName(String name) {
-        this.name = name;
-    }
 
-    public int getId() {
-        return id;
-    }
-
-    public List<User> getUsers() {
-        return users;
-    }
-
-    public void setUsers(List<User> users) {
-        this.users = users;
-    }
-
-    @Override
-    public String toString() {
-        return "Role{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                '}';
-    }
 }
