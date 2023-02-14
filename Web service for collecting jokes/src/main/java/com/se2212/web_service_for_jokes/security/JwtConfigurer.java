7a3f32a260f70b1d16e929a33e73c08f1b057544
PollyBreak <mspolinko@gmail.com>
2023-02-14 10:08:28 +0600

    Create JwtConfigurer.java

---
+++ Web service for collecting jokes/src/main/java/com/se2212/web_service_for_jokes/security/JwtConfigurer.java
@@ -0,0 +1,10 @@
+package com.se2212.web_service_for_jokes.security;
+
+
+import org.springframework.security.config.annotation.SecurityConfigurerAdapter;
+
+public class JwtConfigurer extends SecurityConfigurerAdapter {
+
+
+    
+}
