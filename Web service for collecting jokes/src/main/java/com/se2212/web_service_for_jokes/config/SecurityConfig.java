d57fad8e60317da6624b8b92498815c912452d31
beneft <letun_igor007@mail.ru>
2023-02-14 11:18:04 +0600

    fixed security?

--- Web service for collecting jokes/src/main/java/com/se2212/web_service_for_jokes/config/SecurityConfig.java
+++ Web service for collecting jokes/src/main/java/com/se2212/web_service_for_jokes/config/SecurityConfig.java
@@ -2,22 +2,27 @@ package com.se2212.web_service_for_jokes.config;
 
 import com.se2212.web_service_for_jokes.security.JwtConfigurer;
 import com.se2212.web_service_for_jokes.security.JwtTokenProvider;
+import com.se2212.web_service_for_jokes.security.JwtUserDetailsService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig{
     private final JwtTokenProvider jwtTokenProvider;
+    private final JwtUserDetailsService jwtUserDetailsService;
     private static final String ADMIN_ENDPOINT = "/admin/**";
     private static final String LOGIN_ENDPOINT = "/login";
-    public SecurityConfig(JwtTokenProvider jwtTokenProvider) {
+    public SecurityConfig(JwtTokenProvider jwtTokenProvider,JwtUserDetailsService jwtUserDetailsService) {
         this.jwtTokenProvider = jwtTokenProvider;
+        this.jwtUserDetailsService=jwtUserDetailsService;
     }
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
@@ -30,10 +35,18 @@ public class SecurityConfig{
                 .anyRequest().authenticated()
                 .and()
                 .apply(new JwtConfigurer(jwtTokenProvider));
+        http.authenticationProvider(authProvider());
         return http.build();
     }
     @Bean
     public AuthenticationManager authenticationManager(AuthenticationConfiguration authConfiguration) throws Exception{
         return authConfiguration.getAuthenticationManager();
+    }
+    @Bean
+    public DaoAuthenticationProvider authProvider(){
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+        authProvider.setUserDetailsService(jwtUserDetailsService);
+        authProvider.setPasswordEncoder(jwtTokenProvider.passwordEncoder());
+        return authProvider;
     }
 }
