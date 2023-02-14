c49f04c6166e84598ce8fcef9a2b4d21ed7f3174
PollyBreak <mspolinko@gmail.com>
2023-02-14 10:14:57 +0600

    add field tokenProvider and override void configure

--- Web service for collecting jokes/src/main/java/com/se2212/web_service_for_jokes/security/JwtConfigurer.java
+++ Web service for collecting jokes/src/main/java/com/se2212/web_service_for_jokes/security/JwtConfigurer.java
@@ -2,9 +2,22 @@ package com.se2212.web_service_for_jokes.security;
 
 
 import org.springframework.security.config.annotation.SecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.DefaultSecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
-public class JwtConfigurer extends SecurityConfigurerAdapter {
+public class JwtConfigurer extends SecurityConfigurerAdapter<DefaultSecurityFilterChain, HttpSecurity> {
 
+    private JwtTokenProvider tokenProvider;
 
-    
+    public JwtConfigurer(JwtTokenProvider tokenProvider) {
+        this.tokenProvider = tokenProvider;
+    }
+
+    @Override
+    public void configure(HttpSecurity httpSecurity) throws Exception {
+        JwtTokenFilter tokenFilter = new JwtTokenFilter(tokenProvider);
+        httpSecurity.addFilterBefore(tokenFilter, UsernamePasswordAuthenticationFilter.class);
+
+    }
 }
