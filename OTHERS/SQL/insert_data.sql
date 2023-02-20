a1f807a8359133d13eb0fced9c2c9b93e2a5e4d5
Ban qup <114380823+Ban-qup@users.noreply.github.com>
2023-02-21 00:27:03 +0600

    add some categories

--- OTHERS/SQL/insert_data.sql
+++ OTHERS/SQL/insert_data.sql
@@ -3,6 +3,9 @@ insert into db_for_fun.roles (role) values ('USER'), ('ADMIN');
  
 insert into db_for_fun.user_roles values (1,1);
 insert into db_for_fun.user_roles values (1,2);
+
+insert into category_jokes (name) values ("Каламбуры"), ("Однозначные"), ("Наблюдательные"), ("Сарказм"), ("Остроумие"), ("Физические"), ("Игра Слов"), ("Сатира"), ("Самоирония"), ("Абсурдные"), ("Загадки"), ("Черный юмор");
+
  
  
 commit;
