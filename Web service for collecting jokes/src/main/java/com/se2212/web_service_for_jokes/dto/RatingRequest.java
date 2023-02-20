ce77dd31a1e5898764001f63fd19d69ed41084ca
beneft <letun_igor007@mail.ru>
2023-02-20 22:21:12 +0600

    rating request dto
    the only field "rating" is boolean: 1 for +1 and 0 for -1

---
+++ Web service for collecting jokes/src/main/java/com/se2212/web_service_for_jokes/dto/RatingRequest.java
@@ -0,0 +1,17 @@
+package com.se2212.web_service_for_jokes.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class RatingRequest {
+    private boolean rating;
+    public boolean getRating(){
+        return rating;
+    }
+}
