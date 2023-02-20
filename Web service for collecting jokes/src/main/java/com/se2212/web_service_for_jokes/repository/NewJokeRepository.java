0ceb57b1f624aba4d9d78014b4e32e1a07c9a3da
PollyBreak <mspolinko@gmail.com>
2023-02-20 21:18:16 +0300

    delete useless import

--- Web service for collecting jokes/src/main/java/com/se2212/web_service_for_jokes/repository/NewJokeRepository.java
+++ Web service for collecting jokes/src/main/java/com/se2212/web_service_for_jokes/repository/NewJokeRepository.java
@@ -1,10 +1,8 @@
 package com.se2212.web_service_for_jokes.repository;
 
 import com.se2212.web_service_for_jokes.entity.NewJoke;
-import com.se2212.web_service_for_jokes.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
 
 public interface NewJokeRepository extends JpaRepository<NewJoke,Integer> {
 }
