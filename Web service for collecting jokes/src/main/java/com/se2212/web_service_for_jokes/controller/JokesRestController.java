6ee635c916c5b40bc44a3f71da3da6e30a3b906d
beneft <letun_igor007@mail.ru>
2023-02-20 21:34:11 +0600

    controller update: rating

--- Web service for collecting jokes/src/main/java/com/se2212/web_service_for_jokes/controller/JokesRestController.java
+++ Web service for collecting jokes/src/main/java/com/se2212/web_service_for_jokes/controller/JokesRestController.java
@@ -1,5 +1,6 @@
 package com.se2212.web_service_for_jokes.controller;
 
+import com.se2212.web_service_for_jokes.dto.RatingRequest;
 import com.se2212.web_service_for_jokes.entity.Joke;
 import com.se2212.web_service_for_jokes.entity.JokeCategory;
 import com.se2212.web_service_for_jokes.entity.NewJoke;
@@ -56,6 +57,16 @@ public class JokesRestController {
         Joke joke = jokesService.getJokeById(id);
         return joke;
     }
+
+    @PostMapping("/jokes/{id}")
+    public void rateJoke(@PathVariable int id,
+                         @RequestBody RatingRequest vote)
+    {
+        Joke joke = jokesService.getJokeById(id);
+        joke.updateRating(vote.getRating());
+        jokesService.saveJoke(joke);
+    }
+
     @PostMapping("/jokes")
     public void saveJoke(@RequestBody NewJoke joke){
         newJokeService.saveJoke(joke);
